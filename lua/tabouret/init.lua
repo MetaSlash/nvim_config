901206a20d4604b258964a3eae629e016a9cb26f
Table <Tabouret>
2025-05-23 09:45:18 +0200

    oui

--- lua/tabouret/init.lua
+++ lua/tabouret/init.lua
@@ -6,5 +6,5 @@ require("tabouret.remap")
 
 vim.o.cursorline = true -- Ensure the cursorline is enabled
 vim.cmd([[
-  highlight CursorLine guibg=#1e1e2e guifg=NONE
+highlight CursorLine guibg=#1e1e2e guifg=NONE
 ]])
